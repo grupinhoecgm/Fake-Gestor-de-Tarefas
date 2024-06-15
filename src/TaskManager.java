297688e0a56cfbcbecf6974177cc3d0551da2c9a
grupinhoecgm <ecgmtrabalhos@gmail.com>
2024-06-15 13:39:27 +0100

    só falta de imports.. mais nada

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,3 +1,8 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.time.LocalDateTime;
 import java.util.*;
 
